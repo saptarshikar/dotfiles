20ba27d0e8d451b64fdf4087ace98d0d72366454
Saptarshi Kar <sapkar@amazon.com>
2020-01-19 15:51:13 -0800

    updated install script with oh-my-zsh and powerlevel10k

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 for f in ~/dotfiles/*
 do
   ln -s "$f" "$HOME/.${f##*/}"
